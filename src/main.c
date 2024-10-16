ba5f26028fbfb52d770c1d04df5212ce5536bf51
Davi Herculano <herculanodavi@gmail.com>
2024-10-16 14:09:34 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+
+#define SLEEP_TIME_MS 1000U
+#define LED_NODE      DT_CHOSEN(app_led)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+void main(void)
+{
+    int err = 0;
+
+    if (!gpio_is_ready_dt(&led)) { return; }
+
+    err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (err < 0) { return; }
+
+    while (1)
+    {
+        (void) gpio_pin_toggle_dt(&led);
+        k_msleep(SLEEP_TIME_MS);
+    }
+}
