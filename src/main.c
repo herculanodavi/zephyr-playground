457ac539e732dd56c20f04b82aa9a39f93c90509
Davi Herculano <herculanodavi@gmail.com>
2024-10-17 16:17:11 +0200

    Fix type of main

--- src/main.c
+++ src/main.c
@@ -6,18 +6,20 @@
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
-void main(void)
+int main(void)
 {
     int err = 0;
 
-    if (!gpio_is_ready_dt(&led)) { return; }
+    if (!gpio_is_ready_dt(&led)) { return -1; }
 
     err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (err < 0) { return; }
+    if (err < 0) { return -1; }
 
     while (1)
     {
         (void) gpio_pin_toggle_dt(&led);
         k_msleep(SLEEP_TIME_MS);
     }
+
+    return 0;
 }
