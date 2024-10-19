88166c75fd6a089f76d8460c14b8fe9d8d2333ce
Davi Herculano <herculanodavi@gmail.com>
2024-10-19 19:32:34 +0200

    Add test code for rtc

--- src/main.c
+++ src/main.c
@@ -1,13 +1,14 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/rtc.h>
 #include <zephyr/kernel.h>
 
 #define SLEEP_TIME_MS 1000U
 #define LED_NODE      DT_CHOSEN(app_led)
-#define IMU_NODE      DT_ALIAS(imu_i2c)
+#define RTC_NODE      DT_ALIAS(rtc_node)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-static const struct device* imu_i2c_dev = DEVICE_DT_GET(IMU_NODE);
+static const struct device* rtc_device = DEVICE_DT_GET(RTC_NODE);
 
 int main(void)
 {
@@ -18,10 +19,14 @@ int main(void)
     err = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     if (err < 0) { return -1; }
 
+    struct rtc_time time;
+
     while (1)
     {
         (void) gpio_pin_toggle_dt(&led);
         k_msleep(SLEEP_TIME_MS);
+        rtc_get_time(rtc_device, &time);
+        printk("Current time is: %d:%d", time.tm_hour, time.tm_min);
     }
  
     return 0;
